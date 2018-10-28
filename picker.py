ba3659c1571e539f121ca3f7a495deaa4d90d641
Adam T Johnston <atomjohnston@gmail.com>
2018-10-28 11:41:58 -0400

    separate stat calc from formatting

--- picker.py
+++ picker.py
@@ -35,14 +35,24 @@ from toolz import compose, curry
 from toolz.curried import map
 
 Game = namedtuple('Game', 'key year week away_team away_points home_team home_points')
-Stats = namedtuple('TeamStats', 'team wins losses ties points_for points_against')
+Record = namedtuple('Record', 'team games wins losses ties points_for points_against')
+Stats = namedtuple('Stats', 'team win_per points_per against_per point_delta margin_of_victory simple_ranking')
+Summary = namedtuple('Summary', 'team record stats')
+
+NICKNAMES = frozenset([
+    '49ers', 'bears', 'bengals', 'bills', 'broncos', 'browns', 'buccaneers',
+    'cardinals', 'chargers', 'chiefs', 'colts', 'cowboys', 'dolphins',
+    'eagles', 'falcons', 'giants', 'jaguars', 'jets', 'lions', 'packers',
+    'panthers', 'patriots', 'raiders', 'rams', 'ravens', 'redskins', 'saints',
+    'seahawks', 'steelers', 'texans', 'titans', 'vikings'
+])
 
 
-class TeamStats(Stats):
+class TeamRecord(Record):
     def __add__(self, other):
         if not self.team == other.team:
             raise ValueError('cannot add {} to {}'.format(self.team, other.team))
-        return TeamStats(self.team, *tuple(map(operator.add, self[1:], other[1:])))
+        return TeamRecord(self.team, *tuple(map(operator.add, self[1:], other[1:])))
 
 
 def win_loss_tie(pf, pa):
@@ -52,63 +62,75 @@ def win_loss_tie(pf, pa):
             (0, 1, 0))
 
 
-def update_stats(table, team, pf, pa):
-    try:
-        stats = table[team]
-    except KeyError:
-        stats = blank_team(team)
-    return stats + TeamStats(team, *win_loss_tie(pf, pa), pf, pa)
+def add_stats(stats, team, pf, pa):
+    return stats + TeamRecord(team, 1, *win_loss_tie(pf, pa), pf, pa)
 
 
-def append_game(stat_table, game):
-    stat_table[game.away_team] = update_stats(stat_table, game.away_team, game.away_points, game.home_points)
-    stat_table[game.home_team] = update_stats(stat_table, game.home_team, game.home_points, game.away_points)
-    return stat_table
-
-
-def blank_team(name):
-    return TeamStats(name, *list(repeat(0, 5)))
+def append_game(table, game):
+    table[game.away_team] = add_stats(table[game.away_team], game.away_team, game.away_points, game.home_points)
+    table[game.home_team] = add_stats(table[game.home_team], game.home_team, game.home_points, game.away_points)
+    return table
 
 
 def dict2game(src):
-    src.update({
-        k: int(v)
-        for (k, v) in src.items()
-        if not k.endswith('team')
-    })
+    src.update({k: int(v) for (k, v) in src.items()
+                          if not k.endswith('team')})
     src['key'] = src['year'] * 100 + src['week']
     return Game(**src)
+
+
+def calc(record):
+    delta = record.points_for - record.points_against
+    return Stats(
+        record.team,
+        round(record.wins/record.games, 3),
+        round(record.points_for/record.games, 1),
+        round(record.points_against/record.games, 1),
+        delta,
+        round(delta/record.games, 1),
+        0
+    )
+
+
+def format(record, stats):
+    return {
+        'team': record.team,
+        'w': record.wins,
+        'l': record.losses,
+        't': record.ties,
+        'w%': stats.win_per,
+        'pf': record.points_for,
+        'pf/g': stats.points_per, 
+        'pa': record.points_against,
+        'pa/g': stats.against_per,
+        'pd': stats.point_delta,
+        'mov': stats.margin_of_victory,
+        'srs': stats.simple_ranking
+    }
 
 
 def read_records(year, records):
     return takewhile(lambda row: row['year'] == year, csv.DictReader(records))
 
 
-def get_team_stats(records):
-    read_season_records = curry(read_records)('2018')
-    return reduce(append_game, compose(map(dict2game), read_season_records)(records), {})
+def init_teams():
+    return {team: TeamRecord(team, *list(repeat(0, 6))) for team in NICKNAMES}
 
 
-def calc(stats):
-    delta = stats.points_for - stats.points_against
-    game_count = sum([stats.wins, stats.losses, stats.ties])
-    return {
-        'team': stats.team,
-        'w': stats.wins, 'l': stats.losses, 't': stats.ties,
-        'w%': round(stats.wins/game_count, 3),
-        'pf': stats.points_for, 'pf/g': round(stats.points_for/game_count, 1), 
-        'pa': stats.points_against, 'pa/g': round(stats.points_against/game_count, 1),
-        'pd': delta, 'mov': round(delta/game_count, 1),
-    }
+def get_team_stats(season, records):
+    read_season_records = curry(read_records)(season)
+    games = compose(map(dict2game), read_season_records)(records)
+    records = reduce(append_game, games, init_teams())
+    return {r.team: Summary(r.team, r, calc(r)) for r in records.values()}
 
 
 def run(outfile, opts):
     with open(opts['<records-file>'], 'r') as records:
-        table = get_team_stats(records)
-    outcsv = csv.DictWriter(outfile, fieldnames=['team', 'w', 'l', 't', 'w%', 'pf', 'pf/g', 'pa', 'pa/g', 'pd', 'mov'])
+        table = get_team_stats('2018', records)
+    outcsv = csv.DictWriter(outfile, fieldnames=['team', 'w', 'l', 't', 'w%', 'pf', 'pf/g', 'pa', 'pa/g', 'pd', 'mov', 'srs'])
     outcsv.writeheader()
-    for team_stats in table.values():
-        outcsv.writerow(calc(team_stats))
+    for summary in sorted(table.values(), key=lambda x: x.stats.margin_of_victory, reverse=True):
+        outcsv.writerow(format(summary.record, summary.stats))
 
 
 def main():
